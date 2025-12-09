40ffc03d293bd99943e452f5a06c2cba4d670cf8
Zachary K. <zkesler2027@gmail.com>
2025-12-09 16:49:17 +0000

    Finished

--- CollegeStudent.java
+++ CollegeStudent.java
@@ -1,2 +1,89 @@
-// Create your CollegeStudent class here
+public class CollegeStudent
+{
+    private String name;
+    private String courseTitle;
+    private String courseNumber;
+    private int credits;
+    private int courseFee;
+    private static int CostPerHour = 125;
+    private static int nextStudentID = 1;
+    private int studentID;
+
+
+    public CollegeStudent(String _name, String _courseTitle, String _courseNumber, int _credits)
+    {
+        name = _name;
+        courseTitle = _courseTitle;
+        courseNumber = _courseNumber;
+        credits = _credits;
+        courseFee = _credits * CostPerHour;
+        studentID = nextStudentID;
+        nextStudentID++;
+    }
+    public CollegeStudent()
+    {
+        studentID = nextStudentID;
+        nextStudentID++;
+    }
+    public CollegeStudent(String _name){
+        name = _name;
+        studentID = nextStudentID;
+        nextStudentID++;
+
+    }
+
+    public void setName(String _name){
+        name = _name;
+    }
+    public void setcourseName(String _courseTitle){
+        courseTitle = _courseTitle;
+    }
+    public void setcourseNumber(String _courseNumber){
+        courseNumber = _courseNumber;
+    }
+    public void setCredits(int _credits){
+        credits = _credits;
+    }
+    public void setcourseFee(int _courseFee){
+        courseFee = _courseFee;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getcourseName(){
+        return name;
+    }
+    public String getcourseNumber(){
+        return courseNumber;
+    }
+    public int getCredits(){
+        return credits;
+    }
+    public int getcourseFee(){
+        return courseFee;
+    }
+    public int getstudentID(){
+        return studentID;
+    }
+
+
+    public String toString()
+    {
+        return name + ": " + courseTitle + ", " + courseNumber + ", " + credits + " Cost: $" + courseFee;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}// Create your CollegeStudent class here
 
--- Main.java
+++ Main.java
@@ -5,14 +5,28 @@ class Main {
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in);
-    
+    String ans = "";
     // write a while loop to create and display CollegeStudnets using the below method
-    
+    while (!ans.equals("N")){
+      CollegeStudent student = createStudent(scan);
+      System.out.println(student);
+      System.out.println("Would you like to make a new Student? Y/N");
+      ans = scan.nextLine();
+
+    }
   }
 
   private static CollegeStudent createStudent(Scanner scan) {
     // complete this method to create a CollegeStudent object with details provided by the user
-    
+      System.out.println("What is your first and last name?");
+      String name =  scan.nextLine();
+      System.out.println("What is your course title?");
+      String courseTitle = scan.nextLine();
+      System.out.println("What is your course number?");
+      String courseNumber = scan.nextLine();
+      System.out.println("How many credits do you have?");
+      int credits = scan.nextInt();
+      return new CollegeStudent(name, courseTitle, courseNumber, credits);
   }
     
 }
